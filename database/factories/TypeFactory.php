880fff0e2dc74ad7a8875f4ba8bf6b95d8ade87c
zanza1266 <71390432+zanza1266@users.noreply.github.com>
2021-03-01 20:04:32 +0100

    added types and categories seeders

---
+++ database/factories/TypeFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use Carbon\Carbon;
+
+$factory->define(Type::class, function (Faker $faker) {
+
+    $name = $faker->colorName();
+
+    return [
+        'name' => $name,
+        'created_at' =>Carbon::now()->toDateTimeString()
+    ];
+});
