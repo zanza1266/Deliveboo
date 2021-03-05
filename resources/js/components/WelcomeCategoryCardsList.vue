4c5b28339b99682a5ce3515c1eef58ce3d6114de
Leobar00 <barbierileongmail.com>
2021-03-05 08:33:16 +0200

    commit

--- resources/js/components/WelcomeCategoryCardsList.vue
+++ resources/js/components/WelcomeCategoryCardsList.vue
@@ -15,7 +15,7 @@
         </div>
 
         <div class="row">
-            <div class=" col d-flex justify-content-center flex-nowrap">
+            <div class=" col d-flex justify-content-center ">
                 <div v-for="(category, index) in categoriesList" :key="index" class="card text-center card_style" @click="displayRestaurants(category.id)" >
                     <div class="card-body d-flex justify-content-start align-items-end">
                         <h3 class="card-title"> {{category.name}} </h3>
@@ -29,7 +29,7 @@
                 <ul>
                     <li v-for="(restaurant, index) in displayedRestaurants" :key="index">
                         
-                        <div class="card restaurants-card" style="width: 18rem;">
+                        <div class="card restaurants-card" >
                             <img :src="restaurant.logo" alt="restaurant-logo" class="card-img-top" >
                             <div class="card-body">
                                 <a class="card-title" :href="'restaurant/' + restaurant.id">{{restaurant.name}}</a>
@@ -144,5 +144,12 @@ ul{
             height: 5rem;
         }
     }
+}
+.restaurants-card{
+    width: 18rem;
+    transition: all 1s;
+}
+.restaurants-card:hover{
+    transform: scale(1.2);
 }
 </style>
