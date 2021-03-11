d8fca7db703622691b994aae00acb10065bb127f
Leobar00 <barbierileongmail.com>
2021-03-11 16:24:01 +0200

    welcome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="container">   
         <div class="row">
             <div class="col">
-                <a class="list-restaurant animate__animated animate__zoomIn animate__delay-1s" href="{{route('my-restaurants.index')}}">Vai ai tuoi ristoranti</a>
+                <a class="list-restaurant animate__animated animate__zoomIn animate__delay-1s " href="{{route('my-restaurants.index')}}">Vai ai tuoi ristoranti</a>
             </div>
         </div>
     </div>
